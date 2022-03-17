016a0797576458a31975ef919e7025283d8e72b2
Thomas Vuille <thomas@newtomsoft.fr>
2022-03-17 23:09:32 +0100

    cleanup

--- AvatarGenerator.App/GlobalUsings.cs
+++ AvatarGenerator.App/GlobalUsings.cs
@@ -4,8 +4,6 @@ global using AvatarGenerator.App.WebDriverFactory;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
 global using OpenQA.Selenium;
-global using OpenQA.Selenium.Chrome;
-global using OpenQA.Selenium.Edge;
 global using OpenQA.Selenium.Firefox;
 global using OpenQA.Selenium.Support.UI;
 global using WebDriverManager;
